91dbd54f441e956cab63e0cc7a7b940de154047a
chofodev <diegovalenciavives@gmail.com>
2025-11-19 20:48:37 -0300

    se añade contenido más importante con mayuscula

--- src/pages/Page2.jsx
+++ src/pages/Page2.jsx
@@ -9,7 +9,7 @@ const Page2 = () => {
       
       <main className="grow flex items-center justify-center">
         <h1 className="text-4xl font-bold text-green-700 bg-white p-10 rounded-xl shadow-lg border border-green-100">
-          Hola soy page 2 "EDITAR"
+          Hola soy page 2 "EDITAR JAJAJAJAJA"
         </h1>
       </main>
 
