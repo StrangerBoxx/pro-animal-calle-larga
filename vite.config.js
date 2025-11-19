a6ec4a2530c8f7a35fe6037ddf097ce89263e1f0
chofodev <diegovalenciavives@gmail.com>
2025-11-19 17:10:59 -0300

    fix: base para github pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/pro-animal-calle-larga/',
   plugins: [react(),tailwindcss()],
 })
